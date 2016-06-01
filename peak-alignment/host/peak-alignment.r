f507b505713263691fae442406002cd065e535f1
stephanieherman <stephanie.herman@live.se>
2016-06-01 09:50:20 +0100

    Add and modify Dockerfile, rlibs.sh and Rscript

--- peak-alignment/host/peak-alignment.r
+++ peak-alignment/host/peak-alignment.r
@@ -5,7 +5,9 @@ output = args[2]
 
 load(xset)
 
+library(xcms)
 ### 2.Peaks alignment ###
+print("Retention time correction")
 xset2 <- retcor(xset,  method="obiwarp",plottype="deviation") 
 
 save(xset2,file=output) 
